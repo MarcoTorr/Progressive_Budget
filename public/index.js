bba689244f5afa78a45eb294740513d216633a47
MarcoTorr <marco.torres@aiesec.net>
2021-12-06 21:11:30 -0600

    Halp

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-let transactions = [];
+let transaction = [];
 let myChart;
 
 fetch("/api/transaction")
@@ -16,7 +16,7 @@ fetch("/api/transaction")
   })
   .then(data => {
     // save db data on global variable
-    transactions = data;
+    transaction = data;
 
     populateTotal();
     populateTable();
@@ -25,7 +25,7 @@ fetch("/api/transaction")
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
-  let total = transactions.reduce((total, t) => {
+  let total = transaction.reduce((total, t) => {
     return total + parseInt(t.value);
   }, 0);
 
@@ -37,7 +37,7 @@ function populateTable() {
   let tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
 
-  transactions.forEach(transaction => {
+  transaction.forEach(transaction => {
     // create and populate a table row
     let tr = document.createElement("tr");
     tr.innerHTML = `
@@ -51,7 +51,7 @@ function populateTable() {
 
 function populateChart() {
   // copy array and reverse it
-  let reversed = transactions.slice().reverse();
+  let reversed = transaction.slice().reverse();
   let sum = 0;
 
   // create date labels for chart
